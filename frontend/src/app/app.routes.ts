dc80ca572f9c2577047443584d54641957d15d63
SuryaTech-source <surya608303@gmail.com>
2025-01-25 21:14:54 +0530

    corrected issue

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { CanvasComponent } from './canvas/canvas.component';
 
 export const routes: Routes = [
-    // {   path: '', redirectTo: 'home', pathMatch: 'full' },  
-    // { path: 'home', component: CanvasComponent },
+    {   path: '', redirectTo: 'home', pathMatch: 'full' },  
+    { path: 'home', component: CanvasComponent },
         
 ];
